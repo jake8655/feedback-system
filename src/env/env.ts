ca15c916e5dc7e91a44d4c9672ad416e74b1962e
Dominik Toth <dominik8655@gmail.com>
2023-08-08 17:35:38 +0200

    feat: Working slash commands

--- src/env/env.ts
+++ src/env/env.ts
@@ -2,4 +2,6 @@ import { envsafe, str } from 'envsafe';
 
 export const env = envsafe({
   TOKEN: str(),
+  CLIENT_ID: str(),
+  GUILD_ID: str(),
 });
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,84 @@
 import '@total-typescript/ts-reset';
-import { Client, GatewayIntentBits } from 'discord.js';
+import {
+  Client,
+  Collection,
+  type CommandInteraction,
+  GatewayIntentBits,
+  SlashCommandBuilder,
+  Events,
+  REST,
+  Routes,
+} from 'discord.js';
 import { env } from './env/env';
 
+const commands = [
+  {
+    data: new SlashCommandBuilder()
+      .setName('feedback')
+      .setDescription('Provide feedback to the developers!'),
+    async execute(interaction: CommandInteraction) {
+      await interaction.reply('hehehehaa!');
+    },
+  },
+];
+
+type Command = (typeof commands)[number];
+
 const client = new Client({
-  intents: [GatewayIntentBits.GuildMessages],
+  intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages],
+}) as Client & { commands: Collection<string, Command> };
+
+client.commands = new Collection();
+
+commands.forEach(command => {
+  client.commands.set(command.data.name, command);
 });
 
-client.on('ready', () => {
+client.on('ready', async () => {
   console.log('✅ Ready!');
+
+  await refreshCommands();
 });
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isCommand()) return;
+
+  const command = client.commands.get(interaction.commandName);
+
+  if (!command) return;
+
+  try {
+    await command.execute(interaction);
+  } catch (error) {
+    console.error(error);
+    await interaction.reply({
+      content: 'There was an error while executing this command!',
+      ephemeral: true,
+    });
+  }
+});
+
+const rest = new REST().setToken(env.TOKEN);
+
+const refreshCommands = async () => {
+  try {
+    console.log(
+      `Started refreshing ${commands.length} application (/) commands.`,
+    );
+
+    const data = (await rest.put(
+      Routes.applicationGuildCommands(env.CLIENT_ID, env.GUILD_ID),
+      {
+        body: commands.map(command => command.data.toJSON()),
+      },
+    )) as readonly unknown[];
+
+    console.log(
+      `Successfully reloaded ${data.length} application (/) commands.`,
+    );
+  } catch (error) {
+    console.error(error);
+  }
+};
 
 void client.login(env.TOKEN);
